166735b6a913a311119b049a9c0a8025b6fafc9d
BagoesPantera <bagoespantera01@gmail.com>
2022-06-13 20:11:27 +0800

    init project

---
+++ README.md
@@ -0,0 +1,25 @@
+# Calculator Electron
+
+This calculator made with Electron and React Vite.
+
+## Customize configuration
+
+See [Vite Configuration Reference](https://vitejs.dev/config/).
+
+## Project Setup
+
+```sh
+npm install
+```
+
+### Compile and Hot-Reload for Development
+
+```sh
+npm run dev
+```
+
+### Compile and Minify for Production
+
+```sh
+npm run build
+```
---
+++ src/electron/index.js
@@ -0,0 +1,40 @@
+const path = require("path");
+
+const { app, BrowserWindow } = require("electron");
+const isDev = require("electron-is-dev");
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 300,
+    height: 512,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+
+  win.loadURL(
+    isDev ? "http://localhost:3000" : `file://${path.join(__dirname, "../build/index.html")}`
+  );
+  win.setMenuBarVisibility(false);
+  win.setResizable(false);
+
+  if (isDev) {
+    win.webContents.openDevTools({ mode: "detach" });
+  }
+}
+
+app.whenReady().then(createWindow);
+
+// quit application when all windows are closed, except on macOS
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  // recreate the window if it is not already created
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
